fe4353695b1bced701a21a94c2a442b4530d15da
Mykhailo Kan <mykhailo.kan@giss.pl>
2024-07-14 16:29:27 +0200

    add subnet

--- ec2.tf
+++ ec2.tf
@@ -13,12 +13,13 @@ output "private_key" {
   sensitive = true
 }
 
-resource "aws_instance" "master_node0" {
+resource "aws_instance" "master_node_0" {
   ami           = "ami-0b27735385ddf20e8"
   instance_type = "t3.micro"
   key_name      = aws_key_pair.server_key.key_name
-
+  
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
+  subnet_id = aws_subnet.a.id
   tags = {
     Name = "master_node_0"
   }
@@ -27,6 +28,10 @@ resource "aws_instance" "master_node0" {
     enable_resource_name_dns_aaaa_record = false
     hostname_type                        = "resource-name"
   }
+  metadata_options {
+    http_tokens = "required"
+    http_endpoint = "enabled"
+  }
   user_data = file("./user_data_master.sh")
-  depends_on = [aws_vpc.main, aws_security_group.allow_ssh]
+  depends_on = [aws_vpc.main, aws_security_group.allow_ssh, aws_vpc_security_group_ingress_rule.allow_tls_ipv4, aws_vpc_security_group_egress_rule.allow_all_traffic_ipv4]
 } 
--- vpc.tf
+++ vpc.tf
@@ -6,3 +6,56 @@ resource "aws_vpc" "main" {
     Name = "main"
   }
 }
+
+/* Internet gateway for the public subnet */
+resource "aws_internet_gateway" "ig" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags = {
+    Name        = "subnet-a-igw"
+  }
+}
+
+/* Elastic IP for NAT */
+resource "aws_eip" "nat_eip" {
+  vpc        = true
+  depends_on = [aws_internet_gateway.ig]
+}
+/* NAT */
+resource "aws_nat_gateway" "nat" {
+  allocation_id = "${aws_eip.nat_eip.id}"
+  subnet_id     = "${element(aws_subnet.a.*.id, 0)}"
+  depends_on    = [aws_internet_gateway.ig]
+  tags = {
+    Name = "nat" 
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = "${aws_vpc.main.id}"
+  tags = {
+    Name = "public-route-table"
+  }
+}
+resource "aws_route" "public_internet_gateway" {
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.ig.id}"
+}
+
+resource "aws_subnet" "a" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.0.0/24"
+  map_public_ip_on_launch = true
+  tags = {
+    Name = "subnet_a"
+  }
+}
+
+resource "aws_subnet" "b" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "subnet_b"
+  }
+}
